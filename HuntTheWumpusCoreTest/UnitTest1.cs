d6b5eea41dc785e53c29c20318b4ccb750e71fb3
JamesLilliott <jameslilliott1997@gmail.com>
2023-07-03 22:21:18 +0100

    Added test boilerplate

---
+++ HuntTheWumpusCoreTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace HuntTheWumpusCoreTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
